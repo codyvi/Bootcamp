d3cd0cc81cd1acec16e7df9a81e9b728fecfb824
codyvi <davidcantumartinez@gmail.com>
2019-10-01 15:07:04 -0500

    Started with js

---
+++ Bootcamp/JavaScript/variables.js
@@ -0,0 +1,23 @@
+var name = "Rusty"
+console.log(name)
+name = "Tater"
+console.log(name)
+
+var friend = "Sally"
+
+console.log("Hello there " + friend)
+
+friend = "No one"
+
+console.log("Hello there " + friend)
+
+var num = 43;
+
+console.log(num)
+
+console.log(num + 100)
+
+console.log(num%2)
+
+num = "fifty"
+console.log(num)
