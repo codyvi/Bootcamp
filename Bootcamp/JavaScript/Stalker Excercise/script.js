3f0374e0e0e993aa097ba4a68dd9149972c74332
codyvi <davidcantumartinez@gmail.com>
2019-10-01 15:35:05 -0500

    Stalker excercise

---
+++ Bootcamp/JavaScript/Stalker Excercise/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Give me your first name")
+var lastName = prompt("Give me your last name")
+var userAge = prompt("Give me your age")
+
+console.log("Your full name is " +firstName, " " +lastName)
+console.log("Your are " +userAge, "years old")
