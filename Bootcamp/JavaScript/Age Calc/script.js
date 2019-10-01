48bd697d785101eb3a798d026c7ec64231b93318
codyvi <davidcantumartinez@gmail.com>
2019-10-01 15:44:02 -0500

    Done AgeCalc excercise

---
+++ Bootcamp/JavaScript/Age Calc/script.js
@@ -0,0 +1,5 @@
+var age = prompt("Give me your age")
+
+var days = age * 365.25;
+
+alert(age + " years is roughly " +days)
--- Bootcamp/JavaScript/Stalker Excercise/script.js
+++ Bootcamp/JavaScript/Stalker Excercise/script.js
@@ -1,6 +1,1 @@
-var firstName = prompt("Give me your first name")
-var lastName = prompt("Give me your last name")
-var userAge = prompt("Give me your age")
-
-console.log("Your full name is " +firstName, " " +lastName)
-console.log("Your are " +userAge, "years old")
+var number = prompt("Give me your age")
