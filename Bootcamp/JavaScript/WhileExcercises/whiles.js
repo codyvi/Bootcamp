8fc28de3864b08c87954479282524da9b30d5c79
codyvi <davidcantumartinez@gmail.com>
2019-11-07 17:32:56 -0600

    While Excercise

---
+++ Bootcamp/JavaScript/WhileExcercises/whiles.js
@@ -0,0 +1,47 @@
+console.log("First Excercise");
+
+var num = -10;
+
+while(num <= 19)
+{
+    console.log(num);
+    num++;
+}
+
+console.log("Second Excercise");
+
+var num2 = 10;
+
+while(num2 <= 40)
+{
+    if(num2 % 2 === 0)
+    {
+        console.log(num2);
+    }
+
+    num2++;
+}
+
+console.log("Third Excercise");
+
+var num3 = 300;
+
+while(num3 <= 333)
+{
+    if(num3 % 2 !== 0)
+    {
+        console.log(num3)
+    }
+    num3++;
+}
+
+console.log("Fourth Excercise");
+var num4 = 5;
+while(num4 <= 50)
+{
+    if(num4 % 5 === 0 && num4 % 3 === 0)
+    {
+        console.log(num4);
+    }
+    num4++;
+}
