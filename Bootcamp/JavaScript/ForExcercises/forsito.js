603ddf5c203b9206db3ff4cc929985bd9f87a66d
codyvi <davidcantumartinez@gmail.com>
2019-11-08 16:44:53 -0600

    For's excercise done

--- Bootcamp/JavaScript/ForExcercises/forsito.js
+++ Bootcamp/JavaScript/ForExcercises/forsito.js
@@ -1,8 +1,33 @@
-console.log("First Excercise")
+console.log("First Excercise");
 
 for(var num1 = -10; num1 <= 19; num1++)
 {
     console.log(num1);
 }
 
-console.log("Second Excercise")
+console.log("Second Excercise");
+
+for(var num2 = 10; num2 <= 40; num2+=2)
+{
+    console.log(num2);
+}
+
+console.log("Third Excercise");
+
+for(var num3 = 300; num3 <= 333; num3++)
+{
+    if(num3 % 2 !== 0)
+    {
+        console.log(num3);
+    }
+}
+
+console.log("Fourth Excercise");
+
+for(var num4 = 5; num4 <= 50; num4++)
+{
+    if(num4 % 5 === 0 && num4 % 3 === 0)
+    {
+        console.log(num4);
+    }
+}
