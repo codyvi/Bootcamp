159e1ecfc255d3520ce264674917e66632e25810
codyvi <davidcantumartinez@gmail.com>
2019-11-08 17:26:48 -0600

    Functions Excercises

---
+++ Bootcamp/JavaScript/FunctionsExcercises/fun.js
@@ -0,0 +1,35 @@
+console.log("First Excercise")
+
+function isEven(num)
+{
+    if(num % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;    
+    }
+}
+
+function factorial(num)
+{
+    var result = 1;
+
+    for(var i =  2; i <= num; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
+  
+
+console.log(isEven(4));
+console.log(isEven(21));
+console.log(isEven(68));
+console.log(isEven(333));
+
+console.log("Factorial of five")
+
+console.log(factorial(5))
