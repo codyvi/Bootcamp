bcf69e4318a941eeb38afe595b49b950fcc49e14
codyvi <davidcantumartinez@gmail.com>
2019-11-12 19:52:57 -0600

    Not finished

---
+++ Bootcamp/JavaScript/TodoList/list.js
@@ -0,0 +1,25 @@
+var todos = ["Buy New Turtle"];
+
+window.setTimeout(function() {
+    // put all of your JS code from the lecture here
+    
+      
+    var input = prompt("What would you like to do?");
+    
+    while(input != "quit ")
+    {
+
+      if(input === "list")
+      {
+          console.log(todos);
+      }
+
+      else if(input === "new")
+      {
+        var newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+      }
+    }
+
+  }, 500);
+
