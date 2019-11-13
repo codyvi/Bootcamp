f066c3091f8509c1c48bcd5745cf134884ca4583
codyvi <davidcantumartinez@gmail.com>
2019-11-13 11:45:15 -0600

    Array Excercises Done

---
+++ Bootcamp/JavaScript/ArrayExcercises/arr.js
@@ -0,0 +1,93 @@
+/*
+    Complexity of printReverse: 
+    .reverse() O(N)
+    .forEach() O(N)
+    2N
+    Big-O = O(N)
+*/
+
+function printReverse(array){
+    reversed = array.reverse();
+    reversed.forEach(function(numbers) {
+        console.log(numbers);
+        
+    });
+}
+
+/*
+    Complexity of isUniform: 
+    .forEach() O(N)
+    N
+    Big-O = O(N)
+*/
+
+
+function isUniform(array){
+    var check = array[0];
+    var bool = true;
+    array.forEach(function(letters){
+        if(check !== letters){
+            bool = false;
+        }
+    });
+    return bool;
+}
+
+
+/*
+    Complexity of sumArray: 
+    .forEach() O(N)
+    N
+    Big-O = O(N)
+*/
+
+
+function sumArray(array){
+    var sum = 0;
+    array.forEach(function(numbers){
+        sum += numbers;
+    });
+    return sum;
+}
+
+/*
+    Complexity of max: 
+    .forEach() O(N)
+    N
+    Big-O = O(N)
+*/
+
+function max(array){
+    var max = 0;
+    array.forEach(function(numbers){
+        if(numbers > max){
+            max = numbers;
+        }
+    })
+    return max;
+}
+
+
+
+
+// First Excercise
+console.log("### Reverse Excercise ###")
+var arr1 = ["a", "b", "c"];
+console.log("The array is: " + arr1);
+console.log("Reversing the array!");
+printReverse(arr1);
+// Second Excercise
+console.log("### Uniform Excercise ###")
+var arr2 = ["a","a","a"];
+console.log("The array is: " + arr2)
+console.log(isUniform(arr2));
+//Third Excercise 
+console.log("### Sum Excercise ###")
+var arr3 = [10,3,10,4];
+console.log("The array is: " + arr3)
+console.log(sumArray(arr3));
+//Fourth Excercuse
+console.log("### Max Excercise ###")
+var arr4 = [10,3,10,4];
+console.log("The array is: " + arr4)
+console.log(max(arr4));
