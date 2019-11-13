f55be8aaa42e806e47f380778705c194897a6f91
codyvi <davidcantumartinez@gmail.com>
2019-11-13 10:59:59 -0600

    Finished V2 of todolist

--- Bootcamp/JavaScript/TodoList/list.js
+++ Bootcamp/JavaScript/TodoList/list.js
@@ -3,7 +3,6 @@ var todos = ["Buy New Turtle"];
 window.setTimeout(function() {
     // put all of your JS code from the lecture here
     
-      
     var input = prompt("What would you like to do?");
     
     while(input !== "quit")
@@ -11,18 +10,47 @@ window.setTimeout(function() {
 
       if(input === "list")
       {
-          console.log(todos);
+        listTodos();
       }
 
       else if(input === "new")
       {
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
+        addTodo();
       }
+
+      else if (input === "delete")
+      {
+        deleteTodo();
+      }
+
       input = prompt("What would you like to do?");
     }
 
     console.log("See ya bro")
+    
+    function listTodos()
+    {
+      console.log("*********");
+      todos.forEach(function(todo,index)
+      {
+        console.log(index + ": " + todo);
+      });
+      console.log("*********");
+    }
+
+    function addTodo()
+    {
+      var newTodo = prompt("Enter new todo");
+      todos.push(newTodo);
+      console.log("Added Todo");
+    }
+
+    function deleteTodo()
+    {
+      var indexTodo = prompt("Enter index of todo to delete")
+      todos.splice(indexTodo, 1);
+      console.log("Deleted Todo");
+    }
 
   }, 500);
 
