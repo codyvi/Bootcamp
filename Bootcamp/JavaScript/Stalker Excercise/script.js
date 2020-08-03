471e45c6bbc3a98e78b5472806f70d4b1c621646
David <davidcantumartinez@gmail.com>
2020-08-03 13:39:08 -0500

    Done

--- Bootcamp/JavaScript/Stalker Excercise/script.js
+++ Bootcamp/JavaScript/Stalker Excercise/script.js
@@ -1,1 +1,6 @@
-var number = prompt("Give me your age")
+var number = prompt("Give me your age");
+var Fname = prompt("Give me your First name");
+var Lname = prompt("Give me your Last name");
+
+console.log("Your full name is " + Fname + " " + Lname + " And your age is " + number);
+
